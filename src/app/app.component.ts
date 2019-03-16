628a48347945c2a1c2198ea303563aebb15e1046
Tiep Phan <tieppt12@gmail.com>
2019-03-16 14:33:44 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nxws-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'nx-ws';
+}
